a974ffd45cef4da8124c48a642e97b40efc717ae
VitoriaBLima <168212497+VitoriaBLima@users.noreply.github.com>
2024-08-16 14:44:33 -0300

    kjgldkfgj

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincicpal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-resultado");
